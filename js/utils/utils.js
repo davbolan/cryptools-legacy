3b096a57b0dad7fc676d1942b4a47488ca942f6e
DavidBC <daviletor@gmail.com>
2022-10-12 02:48:21 +0200

    General: Add new fuctions
    Added new fuctions to utils file.

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,5 +1,34 @@
 Array.prototype.on = function (event, func) {
-  for (let i = 0; i < this.length; i++) {
-    this[i].on(event, func);
+  let i = 0;
+  const len = this.length;
+  while (i < len) {
+    this[i++].on(event, func);
   }
+};
+
+Array.prototype.uniq = function () {
+  return [...new Set(this)];
+};
+
+Array.prototype.deleteEmptyValues = function () {
+  return this.filter(Boolean);
+};
+
+String.prototype.delete = function (word) {
+  return this.replace(word, '');
+};
+
+String.prototype.deleteAll = function (word) {
+  return this.replaceAll(word, '');
+};
+
+String.prototype.escapeSpecialChars = function () {
+  return this.replace(/\\n/g, '\\n')
+    .replace(/\\'/g, "\\'")
+    .replace(/\\"/g, '\\"')
+    .replace(/\\&/g, '\\&')
+    .replace(/\\r/g, '\\r')
+    .replace(/\\t/g, '\\t')
+    .replace(/\\b/g, '\\b')
+    .replace(/\\f/g, '\\f');
 };
