2313e415aa33bb86382cb8dbacb69d542ea0c210
DavidBC <daviletor@gmail.com>
2022-10-14 00:41:48 +0200

    Add copypaste button to base64 result
    Add copypaste button and extract the copy functionality in a js module. Created a new error.

--- js/utils/constant.js
+++ js/utils/constant.js
@@ -15,13 +15,28 @@ export const KEY_UP = 'keyup';
 export const KEY_DOWN = 'keydown';
 export const PASTE = 'paste';
 export const FOCUS = 'focus';
+export const SELECT = 'select';
 
+export const JQUERY_EVENTS = {
+  CLICK,
+  CHANGE,
+  KEY_UP,
+  KEY_DOWN,
+  PASTE,
+  FOCUS,
+  SELECT,
+};
 export const CHAR_SEPARATOR = '';
 export const SPACE_SEPARATOR = ' ';
 export const LINE_SEPARATOR = '\n';
 export const DOUBLE_QUOTE = '"';
 export const BACKSLASH = '\\';
 export const ENTER_KEY = 13;
+
+export const CONTENT_TYPE = {
+  TEXT: 'text',
+  VAL: 'val',
+};
 
 // LOOPS
 export const MIN_LOOPS = 2;
@@ -33,4 +48,6 @@ export const ERROR = {
     'El diccionario introducido no tiene un formato Json válido',
   DICT_FIELD_MISSING:
     "El diccionario Json introducido no tiene el campo 'dict'",
+  COPYPASTE_FAILED:
+    'Ha habido un error al copiar al cortapapeles. Por favor, copie el texto manualmente con ctrl+C o cmd+C.',
 };
