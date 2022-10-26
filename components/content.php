d9ddac108536374902739aafef51650ddd89a368
DavidBC <daviletor@gmail.com>
2022-10-26 18:04:21 +0200

    General: Links refactor
    Change relative path to absolute paths

--- components/content.php
+++ components/content.php
@@ -1,12 +1,12 @@
 <main>
 <div class="container my-5">
-  <?php include 'intro.php'?>  
+  <?php include __ROOTPATH__.'/components/intro.php'?>  
   <h2 class="text-center"><?php echo CONTENT_TOOLS?></h2>
   <div class="accordion" id="general-accordion">
-    <?php include 'tools/base64.php'?>
-    <?php include 'tools/deleter.php'?>
-    <?php include 'tools/dictionaryBuilder.php'?>
-    <?php include 'tools/dictionary.php'?>
+    <?php include __ROOTPATH__.'/components/tools/base64.php'?>
+    <?php include __ROOTPATH__.'/components/tools/deleter.php'?>
+    <?php include __ROOTPATH__.'/components/tools/dictionaryBuilder.php'?>
+    <?php include __ROOTPATH__.'/components/tools/dictionary.php'?>
   </div>
 </div>
 </main>
