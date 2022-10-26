d504c1b8065886cafc0a40c6558fda74ff5399dc
DavidBC <daviletor@gmail.com>
2022-10-26 12:54:26 +0200

    Donation: Add QR resources
    - Add easy.qrcode.js
    - Add crypto currencys SVG icons

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <script type="text/javascript" src="js/min/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="js/min/jquery.base64.min.js"></script> 
     <script type="text/javascript" src="js/min/jquery.json-editor.min.js"></script> 
+    <script type="text/javascript" src="js/min/easy.qrcode.min.js"></script> 
     <!--
     <script type="text/javascript" src="js/libs/jquery/jquery.js"></script>
     <script type="text/javascript" src="js/libs/bootstrap/bootstrap.bundle.js"></script>
