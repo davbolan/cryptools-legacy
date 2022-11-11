3124bc4ad8b51ff09b94339417e753602cd69eca
DavidBC <daviletor@gmail.com>
2022-11-11 01:48:37 +0100

    General: Refactor
    Change string template (prefix and suffix) percentage symbol
    to keys

--- components/ui/crypto-address.php
+++ components/ui/crypto-address.php
@@ -1,26 +1,28 @@
 <?php
-$rowTemplate = file_get_contents(ROOT_COMPONENTS_TEMPLATES_PATH.'/row-coin-template.html');
-$jsonCoinsInfo = file_get_contents(ROOT_RESOURCES_PATH.'/coins-info.json');
+$rowTemplate = file_get_contents(ROOT_COMPONENTS_TEMPLATES_PATH . '/row-coin-template.html');
+$jsonCoinsInfo = file_get_contents(ROOT_RESOURCES_PATH . '/coins-info.json');
 $coinsData = json_decode($jsonCoinsInfo, true);
 $coinRows = array();
 foreach ($coinsData as $jsonDataKey => $jsonCoinData) {
-    $coinRow = $rowTemplate;
-    foreach ($jsonCoinData as $coinDataKey => $coinDataValue) {
-        $coinDataKeyUC = strtoupper($coinDataKey);
-        $coinRow = str_replace("%$coinDataKeyUC%", $coinDataValue, $coinRow );
-    }
-    $coinRows[] = $coinRow;
+  $coinRow = $rowTemplate;
+  foreach ($jsonCoinData as $coinDataKey => $coinDataValue) {
+    $coinDataKeyUC = strtoupper($coinDataKey);
+    $coinRow = str_replace("{" . $coinDataKeyUC . "}", $coinDataValue, $coinRow);
   }
+  $coinRows[] = $coinRow;
+}
 $coinRowsStr = implode("", $coinRows)
-?>
+  ?>
 
 <div class="container">
   <div class="row g-2">
-     <?php echo $coinRowsStr; ?>
+    <?php echo $coinRowsStr; ?>
     <div class="col-12 border-top ">
       <div id="qr-cell" class="row d-none qr-cell">
         <div class="d-flex justify-content-center">
-          <span id="coin-name-template" class="d-none"><?php echo DONATION_COIN_ADDRESS?></span>
+          <span id="coin-name-template" class="d-none">
+            <?php echo DONATION_COIN_ADDRESS ?>
+          </span>
           <span id="coin-name">Coin address</span>
         </div>
         <div id="qr-img" class="d-flex justify-content-center qr-container my-2"></div>
@@ -31,8 +33,10 @@ $coinRowsStr = implode("", $coinRows)
         </div>
       </div>
       <div id="qr-label-cell" class="d-flex justify-content-center mt-4 ">
-        <p class="h4 text-justify color-ct-blue-light-variant fw-light fst-italic"><?php echo DONATION_SELECT_CRYPTO?> </p>
-        </div>
+        <p class="h4 text-justify color-ct-blue-light-variant fw-light fst-italic">
+          <?php echo DONATION_SELECT_CRYPTO ?>
+        </p>
+      </div>
     </div>
   </div>
 </div>
--- components/ui/default-dict.php
+++ components/ui/default-dict.php
@@ -5,10 +5,10 @@ $basic      = BUILDER_ADD_DEFAULT_DICT[1];
 $or         = BUILDER_ADD_DEFAULT_DICT[2];
 $extended   = BUILDER_ADD_DEFAULT_DICT[3];
 
-$defaultDictTemplate = str_replace("%ADD_DEFAULT%", $addDefault, $defaultDictTemplate );
-$defaultDictTemplate = str_replace("%BASIC%"      , $basic, $defaultDictTemplate );
-$defaultDictTemplate = str_replace("%OR%"         , $or, $defaultDictTemplate );
-$addDefaultDict      = str_replace("%EXTENDED%"   , $extended, $defaultDictTemplate );
+$defaultDictTemplate = str_replace("{ADD_DEFAULT}", $addDefault, $defaultDictTemplate );
+$defaultDictTemplate = str_replace("{BASIC}"      , $basic, $defaultDictTemplate );
+$defaultDictTemplate = str_replace("{OR}"         , $or, $defaultDictTemplate );
+$addDefaultDict      = str_replace("{EXTENDED}"   , $extended, $defaultDictTemplate );
 
 echo $addDefaultDict;
 ?>
