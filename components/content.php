629e8a35036c546c654b259340b5ba75b1d0d183
DavidBC <daviletor@gmail.com>
2022-11-16 02:25:27 +0100

    General: Minor changes

--- components/content.php
+++ components/content.php
@@ -1,14 +1,15 @@
 <main>
-  <div class="container my-5">
-    <?php include ROOT_COMPONENTS_PATH.'/intro.php' ?>
+  <div class="container mb-5 mt-2">
+    <?php include ROOT_COMPONENTS_PATH . '/intro.php' ?>
     <h2 class="text-center">
       <?php echo $i18n["CONTENT_TOOLS"] ?>
     </h2>
     <div class="accordion" id="general-accordion">
-      <?php include ROOT_COMPONENTS_TOOLS_PATH.'/base64.php' ?>
-      <?php include ROOT_COMPONENTS_TOOLS_PATH.'/deleter.php' ?>
-      <?php include ROOT_COMPONENTS_TOOLS_PATH.'/dictionaryBuilder.php' ?>
-      <?php include ROOT_COMPONENTS_TOOLS_PATH.'/dictionary.php' ?>
+      <?php include ROOT_COMPONENTS_TOOLS_PATH . '/base64.php' ?>
+      <?php include ROOT_COMPONENTS_TOOLS_PATH . '/deleter.php' ?>
+      <?php include ROOT_COMPONENTS_TOOLS_PATH . '/dictionaryBuilder.php' ?>
+      <?php include ROOT_COMPONENTS_TOOLS_PATH . '/dictionary.php' ?>
     </div>
   </div>
+
 </main>
