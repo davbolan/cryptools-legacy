dc8b45bebacdac7883728147cc3eacaf87bcbcfd
DavidBC <daviletor@gmail.com>
2022-11-17 14:40:38 +0100

    General: Refactor META tags

--- index.php
+++ index.php
@@ -11,8 +11,29 @@ require ROOT_I18N_PATH . "/loader.php";
 <html lang="<?php echo $lang; ?>" class="with-scrollbar">
 
 <head>
-  <?php require ROOT_COMPONENTS_PATH . '/meta.php' ?>
+  <title><?php echo WEB_TITLE; ?></title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="<?php echo $web_description; ?>" />
+  <meta http-equiv="Content-Language" content="<?php echo $lang; ?>" />
+  <meta name="distribution" content="global" />
+  <meta name="Robots" content="all" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="<?php echo WEB_TITLE; ?>" />
+  <meta property="og:site_name" content="<?php echo WEB_TITLE; ?>" />
+  <meta property="og:image" content="<?php echo PATH_LOGO; ?>" />
+  <meta property="og:description" content="<?php echo $web_description; ?>" />
+  <meta property="og:url" content="<?php echo WEB_ROOT; ?>" />
+  <meta property="og:locale" content="<?php echo $web_fullLang; ?>" />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:description" content="<?php echo $web_description; ?>" />
+  <meta property="twitter:title" content="<?php echo WEB_TITLE; ?>" />
+  <meta property="twitter:site" content="@Daviletor" />
+  <meta property="twitter:image" content="<?php echo PATH_LOGO; ?>" />
+  <meta property="twitter:creator" content="@Daviletor" />
+  <link rel="canonical" href="<?php echo WEB_ROOT; ?>" />
   <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
   <link rel="stylesheet" href="/css/bootstrap/bootstrap.css" />
   <link rel="stylesheet" href="/css/bootstrap-icons/bootstrap-icons.min.css" />
   <!--
